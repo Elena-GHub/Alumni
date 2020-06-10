af6d82302fe808493f262b54d3ab2183be566bb5
Elena <cabjelen@yahoo.es>
2020-06-10 12:07:20 +0200

    Elena/corrections of gitignore added

---
+++ app/Forum.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Forum extends Model
+{
+    public function category()
+    {
+        return $this->belongsTo('App\Category', 'category_id');
+    }
+
+    public function threads()
+    {
+        return $this->hasMany('App\Thread', 'forum_id');
+    }
+}
---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Category; 
+use App\Forum;
+use App\Post;
+use App\User;
+
+class CategoryController extends Controller
+{
+    public function getCategories(Request $request)
+    {
+        $categories = Category::with('fora')->get();
+
+        for ($i = 0; $i < count($categories); $i++)
+        {
+            $currentCategory = $categories[$i];
+
+            for ($j = 0; $j < count($currentCategory['fora']); $j++)
+            {
+                $currentForum = $currentCategory['fora'][$j];
+                
+                $currentForum['latest'] = Post::leftJoin('threads', 'posts.thread_id', '=', 'threads.id')
+                ->where('threads.forum_id', '=', $currentForum->id)
+                ->latest()
+                ->select('posts.*', 'threads.title as thread_title', 'threads.id as thread.id')
+                ->first();
+                
+                $currentForum['latest']['user'] = User::find($currentForum['latest']->user_id);
+               
+                $currentForum['replies'] = Post::leftJoin('threads', 'posts.thread_id', '=', 'threads.id')
+                ->where('threads.forum_id', '=', $currentForum->id)
+                ->count();
+            }
+        }
+        
+        return response()->json($categories, 200);
+    }
+}
